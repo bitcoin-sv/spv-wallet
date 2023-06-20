1f5e2b380e285a62b987c308e255651cad4df1ef
mrz1836 <mrz1818@gmail.com>
2023-06-20 06:47:44 -0400

    Bumped version

--- config/config.go
+++ config/config.go
@@ -28,7 +28,7 @@ const (
 	EnvironmentStaging             = "staging"
 	EnvironmentTest                = "test"
 	HealthRequestPath              = "health"
-	Version                        = "v0.5.1"
+	Version                        = "v0.5.2"
 )
 
 // Local variables for configuration
