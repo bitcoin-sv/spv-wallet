317e5e2456515502f1a2212cce5ac86d9063e1ef
mrz1836 <mrz1818@gmail.com>
2023-07-12 09:00:25 -0400

    Bumped version

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -19,7 +19,7 @@ import (
 
 // main method starts everything for the BUX Server
 // @title           BUX: Server
-// @version         v0.5.4
+// @version         v0.5.5
 // @securityDefinitions.apikey bux-auth-xpub
 // @in header
 // @name bux-auth-xpub
--- config/config.go
+++ config/config.go
@@ -28,7 +28,7 @@ const (
 	EnvironmentStaging             = "staging"
 	EnvironmentTest                = "test"
 	HealthRequestPath              = "health"
-	Version                        = "v0.5.4"
+	Version                        = "v0.5.5"
 )
 
 // Local variables for configuration
