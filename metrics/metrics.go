55d0fb5e3368eae0496bda09885f0572d8f82938
Krzysztof Tomecki <152964795+chris-4chain@users.noreply.github.com>
2024-02-12 07:18:40 +0100

    refactor(BUX-566): fix lint errors

--- metrics/metrics.go
+++ metrics/metrics.go
@@ -7,11 +7,13 @@ import (
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
+// Metrics is the metrics collector
 type Metrics struct {
 	gatherer   prometheus.Gatherer
 	registerer prometheus.Registerer
 }
 
+// newMetrics will create a new Metrics object; this is private to ensure only one, global instance is created
 func newMetrics() *Metrics {
 	registry := prometheus.NewRegistry()
 	constLabels := prometheus.Labels{"app": appName}
@@ -25,6 +27,7 @@ func newMetrics() *Metrics {
 	return m
 }
 
-func (m *Metrics) HttpHandler() http.Handler {
+// HTTPHandler will return the http.Handler for the metrics
+func (m *Metrics) HTTPHandler() http.Handler {
 	return promhttp.HandlerFor(metrics.gatherer, promhttp.HandlerOpts{Registry: metrics.registerer})
 }
