47637dff48d3de9009a9dcf74f66e135bc0fac1f
jakubmkowalski <jakub.kowalski@4chain.studio>
2024-02-21 13:30:03 +0100

    chore: renames bux_... files, capital W i Wallet

--- mappings/common/common.go
+++ mappings/common/common.go
@@ -20,7 +20,7 @@ func MapToContract(m *engine.Model) *common.Model {
 	}
 }
 
-// MapToModel will map the spv-wallet-models contract to the common SPV Model
+// MapToModel will map the spv-wallet-models contract to the common SPV Wallet Model
 func MapToModel(m *common.Model) *engine.Model {
 	if m == nil {
 		return nil
