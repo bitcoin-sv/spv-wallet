ea7b3083a0774014da2031e01a2d5953cf1a36d7
Krzysztof Tomecki <152964795+chris-4chain@users.noreply.github.com>
2024-05-29 10:16:56 +0200

    feat(SPV-737): Conditions as pointer

--- models/filter/generics.go
+++ models/filter/generics.go
@@ -3,7 +3,7 @@ package filter
 // ConditionsModel is a generic model for handling conditions with metadata
 type ConditionsModel[TFilter any] struct {
 	// Custom conditions used for filtering the search results. Every field within the object is optional.
-	Conditions TFilter `json:"conditions"`
+	Conditions *TFilter `json:"conditions"`
 	// Accepts a JSON object for embedding custom metadata, enabling arbitrary additional information to be associated with the resource
 	Metadata *map[string]interface{} `json:"metadata,omitempty" swaggertype:"object,string" example:"key:value,key2:value2"`
 }
