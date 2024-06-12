0b87d21d58f4106895931c1232943e296be7b1a2
wregulski <wojciechregulski@outlook.com>
2024-06-12 09:43:39 +0200

    fix(SPV-871): change the order of struct to let it be compiled to smaller chunk of data

--- config/config.go
+++ config/config.go
@@ -85,12 +85,12 @@ type CacheConfig struct {
 
 // CallbackConfig is the configuration for callbacks
 type CallbackConfig struct {
-	// Enabled is the flag that enables callbacks.
-	Enabled bool `json:"enabled" mapstructure:"enabled"`
 	// Host is the URL for broadcast callback registration.
 	Host string `json:"callback_host" mapstructure:"callback_host"`
 	// Token is the token for broadcast callback registration.
 	Token string `json:"callback_token" mapstructure:"callback_token"`
+	// Enabled is the flag that enables callbacks.
+	Enabled bool `json:"enabled" mapstructure:"enabled"`
 }
 
 // ClusterConfig is a configuration for the SPV Wallet cluster
