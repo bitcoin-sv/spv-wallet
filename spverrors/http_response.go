a3eecee3a40ad7d15f621fb82778580ab06ec699
Paweł Lewandowski <plewandowski555@gmail.com>
2024-06-20 16:08:27 +0200

    chore(SPV-846): update SPVError structure

--- spverrors/http_response.go
+++ spverrors/http_response.go
@@ -1,38 +1,54 @@
 package spverrors
 
 import (
+	"errors"
 	"github.com/gin-gonic/gin"
 	"github.com/rs/zerolog"
 )
 
-type Response struct {
+// SPVError is extended error which holds information about http status and code that should be returned
+type SPVError struct {
+	Code       string
+	Message    string
+	StatusCode int
+}
+
+// responseError is an error which will be returned in HTTP response
+type responseError struct {
 	Code    string `json:"Code"`
 	Message string `json:"Message"`
+}
+
+func (e SPVError) Error() string {
+	return e.Message
 }
 
 // ErrorResponse is searching for error and setting it up in gin context
 func ErrorResponse(c *gin.Context, err error, log *zerolog.Logger) {
 	response, statusCode, found := getError(err)
-	if !found && log != nil {
-		log.Warn().Str("module", "spv-errors").Msgf("Unable to get information about error, details:  %s", err.Error())
-	}
+	logError(found, log, err)
 	c.JSON(statusCode, response)
 }
 
 // AbortWithErrorResponse is searching for error and abort with error set
 func AbortWithErrorResponse(c *gin.Context, err error, log *zerolog.Logger) {
 	response, statusCode, found := getError(err)
-	if !found && log != nil {
-		log.Warn().Str("module", "spv-errors").Msgf("Unable to get information about error, details:  %s", err.Error())
-	}
+	logError(found, log, err)
 	c.AbortWithStatusJSON(statusCode, response)
 }
 
-func getError(err error) (Response, int, bool) {
-	errDetails, ok := SPVErrorResponses[err]
+func getError(err error) (responseError, int, bool) {
+	var errDetails SPVError
+	ok := errors.As(err, &errDetails)
 	if !ok {
-		return Response{Code: UnknownError, Message: "Unable to get information about error"}, 500, false
+		return responseError{Code: "error-unknown", Message: "Unable to get information about error"}, 500, false
 	}
 
-	return Response{Code: errDetails.Code, Message: errDetails.Message}, errDetails.StatusCode, true
+	return responseError{Code: errDetails.Code, Message: errDetails.Message}, errDetails.StatusCode, true
+}
+
+func logError(found bool, log *zerolog.Logger, err error) {
+	if !found && log != nil {
+		log.Warn().Str("module", "spv-errors").Msgf("Unable to get information about error, details:  %s", err.Error())
+	}
 }
