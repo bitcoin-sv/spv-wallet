54ab00da2d0d0fa2f33ae7b993a1fc405c217a5c
Paweł Lewandowski <plewandowski555@gmail.com>
2024-07-31 10:45:57 +0200

    feat(SPV-847): add option to wrap defined errors with cause

--- models/errors.go
+++ models/errors.go
@@ -1,10 +1,15 @@
 package models
 
+import (
+	"github.com/pkg/errors"
+)
+
 type ExtendedError interface {
 	error
 	GetCode() string
 	GetMessage() string
 	GetStatusCode() int
+	StackTrace() (trace errors.StackTrace)
 }
 
 // SPVError is extended error which holds information about http status and code that should be returned
@@ -12,6 +17,11 @@ type SPVError struct {
 	Code       string
 	Message    string
 	StatusCode int
+	cause      error
+}
+
+type stackTracer interface {
+	StackTrace() errors.StackTrace
 }
 
 // ResponseError is an error which will be returned in HTTP response
@@ -40,4 +50,34 @@ func (e SPVError) GetMessage() string {
 // GetStatusCode returns the error status code for SPVError
 func (e SPVError) GetStatusCode() int {
 	return e.StatusCode
+}
+
+// StackTrace returns the error's stack trace.
+func (e SPVError) StackTrace() (trace errors.StackTrace) {
+	err, ok := e.cause.(stackTracer)
+	if !ok {
+		return
+	}
+
+	trace = err.StackTrace()
+
+	return
+}
+
+func (e SPVError) Unwrap() error {
+	return e.cause
+}
+
+func (e SPVError) Wrap(err error) SPVError {
+	e.cause = err
+	return e
+}
+
+func (e SPVError) WithTrace(err error) SPVError {
+	if st := stackTracer(nil); !errors.As(e.cause, &st) {
+		return e.Wrap(errors.WithStack(err))
+	} else {
+		return e.Wrap(err)
+	}
+	//return e
 }
