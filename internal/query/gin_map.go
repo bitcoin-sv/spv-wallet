ddffd09e2dd1e46df6bf5b84928b7b2df7a9221d
Damian Orzepowski <damian.orzepowski@4chain.studio>
2024-08-14 21:16:05 +0200

    chore(SPV-793): disable linter because we want to mimic gin API

--- internal/query/gin_map.go
+++ internal/query/gin_map.go
@@ -4,6 +4,8 @@ import "github.com/gin-gonic/gin"
 
 // QueryNestedMap returns a map for a given query key.
 // In contrast to QueryMap it handles nesting in query maps like key[foo][bar]=value.
+//
+//revive:disable:exported We want to mimic the gin API
 func QueryNestedMap(c *gin.Context, key string) (dicts map[string]interface{}) {
 	dicts, _ = GetQueryNestedMap(c, key)
 	return
@@ -16,3 +18,5 @@ func GetQueryNestedMap(c *gin.Context, key string) (map[string]interface{}, bool
 	q := c.Request.URL.Query()
 	return GetMap(q, key)
 }
+
+//revive:enable:exported
