36ef9c93daae3d5f3eaf02a17da1c8695877bece
jakubmkowalski <jakub.kowalski@4chain.studio>
2024-09-05 10:56:27 +0200

    fix(SPV-996): remove unnecessary conversion

--- mappings/transaction.go
+++ mappings/transaction.go
@@ -26,7 +26,7 @@ func MapToTransactionContract(t *engine.Transaction) *response.Transaction {
 		NumberOfOutputs:      t.NumberOfOutputs,
 		DraftID:              t.DraftID,
 		TotalValue:           t.TotalValue,
-		Status:               string(t.TxStatus),
+		Status:               t.TxStatus,
 		TransactionDirection: string(t.Direction),
 	}
 
