c6fbd58afbcbd50f1e59a717b9810868d619ec19
Krzysztof Tomecki <152964795+chris-4chain@users.noreply.github.com>
2024-09-24 14:52:01 +0200

    feat(SPV-1072): error test cases defined in a map

--- engine/spverrors/unfold_test.go
+++ engine/spverrors/unfold_test.go
@@ -15,36 +15,40 @@ func TestUnfoldError(t *testing.T) {
 	err2 := models.SPVError{Code: "test-err2", Message: "test error2", StatusCode: 400}
 	err3 := models.SPVError{Code: "test-err3", Message: "test error3", StatusCode: 500}
 
-	t.Run("unfold single string error", func(t *testing.T) {
-		err := errors.New("test error")
-		unfolded := UnfoldError(err)
-		require.Equal(t, "[*errors.errorString] test error", unfolded)
-	})
+	testCases := map[string]struct {
+		input    error
+		expected string
+	}{
+		"unfold single string error": {
+			input:    errors.New("test error"),
+			expected: "[*errors.errorString] test error",
+		},
+		"unfold single SPVError": {
+			input:    err1,
+			expected: "[models.SPVError(404)] test error1",
+		},
+		"unfold SPVError wrapped in SPVError": {
+			input:    err1.Wrap(err2),
+			expected: "[models.SPVError(404)] test error1 -> [models.SPVError(400)] test error2",
+		},
+		"unfold SPVError wrapped by pkg.Wrap and then wrapped into SPVError": {
+			input:    err1.Wrap(pkgerrors.Wrap(err3, "test error2")),
+			expected: "[models.SPVError(404)] test error1 -> [*errors.withStack] test error2: test error3 -> [*errors.withMessage] test error2: test error3 -> [models.SPVError(500)] test error3",
+		},
+		"wrapping with fmt.Errorf": {
+			input:    fmt.Errorf("Some error is here %w - in the middle of a string", err1),
+			expected: "[*fmt.wrapError] Some error is here test error1 - in the middle of a string -> [models.SPVError(404)] test error1",
+		},
+		"joining errors": {
+			input:    err1.Wrap(errors.Join(err2, fmt.Errorf("test error3"))),
+			expected: "[models.SPVError(404)] test error1 -> [*errors.joinError] ([models.SPVError(400)] test error2 AND [*errors.errorString] test error3)",
+		},
+	}
 
-	t.Run("unfold single SPVError", func(t *testing.T) {
-		unfolded := UnfoldError(err1)
-		require.Equal(t, "[models.SPVError(404)] test error1", unfolded)
-	})
-
-	t.Run("unfold SPVError wrapped in SPVError", func(t *testing.T) {
-		err := err1.Wrap(err2)
-		unfolded := UnfoldError(err)
-		require.Equal(t, "[models.SPVError(404)] test error1 -> [models.SPVError(400)] test error2", unfolded)
-	})
-	t.Run("unfold SPVError wrapped by pkg.Wrap and then wrapped into SPVError", func(t *testing.T) {
-		err := err1.Wrap(pkgerrors.Wrap(err3, "test error2"))
-		unfolded := UnfoldError(err)
-		require.Equal(t, "[models.SPVError(404)] test error1 -> [*errors.withStack] test error2: test error3 -> [*errors.withMessage] test error2: test error3 -> [models.SPVError(500)] test error3", unfolded)
-	})
-	t.Run("wrapping with fmt.Errorf", func(t *testing.T) {
-		err := fmt.Errorf("Some error is here %w - in the middle of a string", err1)
-		unfolded := UnfoldError(err)
-		require.Equal(t, "[*fmt.wrapError] Some error is here test error1 - in the middle of a string -> [models.SPVError(404)] test error1", unfolded)
-	})
-
-	t.Run("joining errors", func(t *testing.T) {
-		err := err1.Wrap(errors.Join(err2, fmt.Errorf("test error3")))
-		unfolded := UnfoldError(err)
-		require.Equal(t, "[models.SPVError(404)] test error1 -> [*errors.joinError] ([models.SPVError(400)] test error2 AND [*errors.errorString] test error3)", unfolded)
-	})
+	for name, tc := range testCases {
+		t.Run(name, func(t *testing.T) {
+			unfolded := UnfoldError(tc.input)
+			require.Equal(t, tc.expected, unfolded)
+		})
+	}
 }
