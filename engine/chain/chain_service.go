62ba56ca58c81a148fd97e59d0518d87f764755e
chris-4chain <152964795+chris-4chain@users.noreply.github.com>
2024-10-04 10:39:30 +0200

    refactor(SPV-1061): arc requests moved to a single package (#721)

--- engine/chain/chain_service.go
+++ engine/chain/chain_service.go
@@ -1,22 +1,19 @@
 package chain
 
 import (
-	"github.com/bitcoin-sv/spv-wallet/engine/chain/internal/policy"
-	"github.com/bitcoin-sv/spv-wallet/engine/chain/internal/query"
+	"github.com/bitcoin-sv/spv-wallet/engine/chain/internal/arc"
 	"github.com/bitcoin-sv/spv-wallet/engine/chain/models"
 	"github.com/go-resty/resty/v2"
 	"github.com/rs/zerolog"
 )
 
 type chainService struct {
-	QueryService
-	PolicyService
+	ARCService
 }
 
 // NewChainService creates a new chain service.
 func NewChainService(logger zerolog.Logger, httpClient *resty.Client, arcCfg chainmodels.ARCConfig) Service {
 	return &chainService{
-		query.NewQueryService(logger.With().Str("chain", "query").Logger(), httpClient, arcCfg),
-		policy.NewPolicyService(logger.With().Str("chain", "policy").Logger(), httpClient, arcCfg),
+		arc.NewARCService(logger.With().Str("chain", "arc").Logger(), httpClient, arcCfg),
 	}
 }
--- engine/chain/interface.go
+++ engine/chain/interface.go
@@ -6,18 +6,13 @@ import (
 	"github.com/bitcoin-sv/spv-wallet/engine/chain/models"
 )
 
-// QueryService for querying transactions.
-type QueryService interface {
+// ARCService for querying ARC server.
+type ARCService interface {
 	QueryTransaction(ctx context.Context, txID string) (*chainmodels.TXInfo, error)
-}
-
-// PolicyService for querying policy.
-type PolicyService interface {
 	GetPolicy(ctx context.Context) (*chainmodels.Policy, error)
 }
 
 // Service related to the chain.
 type Service interface {
-	QueryService
-	PolicyService
+	ARCService
 }
