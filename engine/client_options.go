8d79218578e0b23a17bed6884ab05d8684025442
Damian Orzepowski <damian.orzepowski@4chain.studio>
2024-10-10 13:56:23 +0200

    test(SPV-1086): fix linter

--- engine/client_options.go
+++ engine/client_options.go
@@ -15,12 +15,11 @@ import (
 	"github.com/bitcoin-sv/spv-wallet/engine/datastore"
 	"github.com/bitcoin-sv/spv-wallet/engine/logging"
 	"github.com/bitcoin-sv/spv-wallet/engine/metrics"
-	"github.com/go-resty/resty/v2"
-
 	"github.com/bitcoin-sv/spv-wallet/engine/taskmanager"
 	"github.com/bitcoin-sv/spv-wallet/engine/utils"
 	"github.com/coocood/freecache"
 	"github.com/go-redis/redis/v8"
+	"github.com/go-resty/resty/v2"
 	"github.com/mrz1836/go-cache"
 	"github.com/mrz1836/go-cachestore"
 	"github.com/rs/zerolog"
