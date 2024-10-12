9d6cd362b1ad2223968a8e64d2d2f6d68ee61bb9
wregulski <wojciechregulski@outlook.com>
2024-10-12 14:46:48 +0200

    fix(SPV-898): wrap errors in decrypt func in a correct way

--- engine/utils/encrypt.go
+++ engine/utils/encrypt.go
@@ -9,16 +9,14 @@ import (
 // Encrypt will encrypt the value using the encryption key
 func Encrypt(encryptionKey, encryptValue string) (string, error) {
 	var err error
-	var privateKey *ec.PrivateKey
 	// Get the keys seeded with the encryption key
-	privateKey, err = ec.PrivateKeyFromHex(encryptionKey)
+	privateKey, err := ec.PrivateKeyFromHex(encryptionKey)
 	if err != nil {
 		return "", spverrors.Wrapf(err, "error getting private keys from encryption key")
 	}
 
 	// Encrypt the private key
-	var encryptedValue string
-	encryptedValue, err = ecies.EncryptSingle(encryptValue, privateKey)
+	encryptedValue, err := ecies.EncryptSingle(encryptValue, privateKey)
 	if err != nil {
 		return "", spverrors.Wrapf(err, "error encrypting data with private key")
 	}
@@ -29,6 +27,14 @@ func Encrypt(encryptionKey, encryptValue string) (string, error) {
 // Decrypt will take the data and decrypt using a char(64) key
 func Decrypt(encryptionKey, data string) (string, error) {
 	privateKey, err := ec.PrivateKeyFromHex(encryptionKey)
+	if err != nil {
+		return "", spverrors.Wrapf(err, "error getting private keys from encryption key")
+	}
+
 	keyString, err := ecies.DecryptSingle(data, privateKey)
-	return keyString, spverrors.Wrapf(err, "error decrypting data with private key")
+	if err != nil {
+		return "", spverrors.Wrapf(err, "error decrypting data with private key")
+	}
+
+	return keyString, nil
 }
