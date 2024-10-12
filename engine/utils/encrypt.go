4d84659ac0bb5ab74f492f069bd84ade4f24ff0b
wregulski <wojciechregulski@outlook.com>
2024-10-12 14:43:26 +0200

    fix(SPV-898): avoid ineffectual assignment to err (ineffassign)

--- engine/utils/encrypt.go
+++ engine/utils/encrypt.go
@@ -8,14 +8,17 @@ import (
 
 // Encrypt will encrypt the value using the encryption key
 func Encrypt(encryptionKey, encryptValue string) (string, error) {
+	var err error
+	var privateKey *ec.PrivateKey
 	// Get the keys seeded with the encryption key
-	privateKey, err := ec.PrivateKeyFromHex(encryptionKey)
+	privateKey, err = ec.PrivateKeyFromHex(encryptionKey)
 	if err != nil {
 		return "", spverrors.Wrapf(err, "error getting private keys from encryption key")
 	}
 
 	// Encrypt the private key
-	encryptedValue, err := ecies.EncryptSingle(encryptValue, privateKey)
+	var encryptedValue string
+	encryptedValue, err = ecies.EncryptSingle(encryptValue, privateKey)
 	if err != nil {
 		return "", spverrors.Wrapf(err, "error encrypting data with private key")
 	}
