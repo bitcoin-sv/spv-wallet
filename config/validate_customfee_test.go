044bd5feeedfd66016bc546c5837755ad7a14495
Krzysztof Tomecki <152964795+chris-4chain@users.noreply.github.com>
2024-10-21 09:10:35 +0200

    feat(SPV-1075): adjust validate_customfee_test to use "scenario"

--- config/validate_customfee_test.go
+++ config/validate_customfee_test.go
@@ -8,59 +8,91 @@ import (
 )
 
 func TestValidateFeeUnit(t *testing.T) {
-	tests := map[string]struct {
-		customFee  *config.FeeUnitConfig
-		shouldFail bool
+	validConfigTests := map[string]struct {
+		scenario func(cfg *config.AppConfig)
 	}{
 		"Not defined is valid": {
-			customFee: nil,
+			scenario: func(cfg *config.AppConfig) {
+				cfg.CustomFeeUnit = nil
+			},
 		},
 		"Standard": {
-			customFee: &config.FeeUnitConfig{
-				Satoshis: 1,
-				Bytes:    1000,
+			scenario: func(cfg *config.AppConfig) {
+				cfg.CustomFeeUnit = &config.FeeUnitConfig{
+					Satoshis: 1,
+					Bytes:    1000,
+				}
 			},
 		},
 		"Zero Satoshi is valid": {
-			customFee: &config.FeeUnitConfig{
-				Satoshis: 0,
-				Bytes:    1000,
+			scenario: func(cfg *config.AppConfig) {
+				cfg.CustomFeeUnit = &config.FeeUnitConfig{
+					Satoshis: 0,
+					Bytes:    1000,
+				}
 			},
-		},
-		"Empty is not ok": {
-			customFee:  &config.FeeUnitConfig{},
-			shouldFail: true,
-		},
-		"Negative satoshis": {
-			customFee: &config.FeeUnitConfig{
-				Satoshis: -1,
-				Bytes:    1000,
-			},
-			shouldFail: true,
-		},
-		"Zero bytes": {
-			customFee: &config.FeeUnitConfig{
-				Satoshis: 1,
-				Bytes:    0,
-			},
-			shouldFail: true,
-		},
-		"Negative bytes": {
-			customFee: &config.FeeUnitConfig{
-				Satoshis: 1,
-				Bytes:    -1,
-			},
-			shouldFail: true,
 		},
 	}
-	for name, test := range tests {
+	for name, test := range validConfigTests {
 		t.Run(name, func(t *testing.T) {
-			err := test.customFee.Validate()
-			if test.shouldFail {
-				require.Error(t, err)
-			} else {
-				require.NoError(t, err)
-			}
+			// given:
+			cfg := config.GetDefaultAppConfig()
+
+			test.scenario(cfg)
+
+			// when:
+			err := cfg.Validate()
+
+			// then:
+			require.NoError(t, err)
+		})
+	}
+
+	invalidConfigTests := map[string]struct {
+		scenario func(cfg *config.AppConfig)
+	}{
+		"Empty is not ok": {
+			scenario: func(cfg *config.AppConfig) {
+				cfg.CustomFeeUnit = &config.FeeUnitConfig{}
+			},
+		},
+		"Negative satoshis": {
+			scenario: func(cfg *config.AppConfig) {
+				cfg.CustomFeeUnit = &config.FeeUnitConfig{
+					Satoshis: -1,
+					Bytes:    1000,
+				}
+			},
+		},
+		"Zero bytes": {
+			scenario: func(cfg *config.AppConfig) {
+				cfg.CustomFeeUnit = &config.FeeUnitConfig{
+					Satoshis: 1,
+					Bytes:    0,
+				}
+			},
+		},
+		"Negative bytes": {
+			scenario: func(cfg *config.AppConfig) {
+				cfg.CustomFeeUnit = &config.FeeUnitConfig{
+					Satoshis: 1,
+					Bytes:    -1,
+				}
+			},
+		},
+	}
+	for name, test := range invalidConfigTests {
+		t.Run(name, func(t *testing.T) {
+			// given:
+			cfg := config.GetDefaultAppConfig()
+
+			test.scenario(cfg)
+
+			// when:
+			err := cfg.Validate()
+
+			// then:
+			require.Error(t, err)
 		})
 	}
 }
