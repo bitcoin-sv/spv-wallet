9f92ca71e3f8ed27dbfd4769688a031013193dbb
Krzysztof Tomecki <152964795+chris-4chain@users.noreply.github.com>
2024-11-07 08:30:39 +0100

    feat(SPV-1169): mock TX generator

---
+++ engine/tester/fixtures/tx_fixtures.go
@@ -0,0 +1,227 @@
+package fixtures
+
+import (
+	"testing"
+
+	bip32 "github.com/bitcoin-sv/go-sdk/compat/bip32"
+	"github.com/bitcoin-sv/go-sdk/script"
+	trx "github.com/bitcoin-sv/go-sdk/transaction"
+	sighash "github.com/bitcoin-sv/go-sdk/transaction/sighash"
+	"github.com/bitcoin-sv/go-sdk/transaction/template/p2pkh"
+	"github.com/bitcoin-sv/spv-wallet/models/bsv"
+)
+
+const (
+	// XPrivStr is the xpriv key which can be safely used for testing purposes
+	XPrivStr = "xprv9s21ZrQH143K2mYcVbQKRnVp8r8cVUvNVkfk7wvMd9rPxf86SoW4dJTH7Zduta6tuYS1XEKTb8hxK4zuz2fSVW9iikoRcWor8d2mGjtmke5"
+)
+
+// Fixtures for testing purposes
+var (
+	XPriv, _                        = bip32.GenerateHDKeyFromString(XPrivStr)
+	Priv, _                         = bip32.GetPrivateKeyFromHDKey(XPriv)
+	Pub                             = Priv.PubKey()
+	Address, _                      = script.NewAddressFromPublicKey(Pub, true)
+	P2PKHLockingScript, _           = p2pkh.Lock(Address)
+	P2PKHUnlockingScriptTemplate, _ = p2pkh.Unlock(Priv, ptr(sighash.AllForkID))
+)
+
+// grandparentTXIDs are used to indicate prevTXID for parentTXs(source transactions)
+// [grandparentTX] -> [parentTX] -> [tx]
+// tx is the actual transaction that is being created
+// parentTX txs contains merkle proof
+// parentOfSource txs are just placeholders (IDs only)
+var grandparentTXIDs = []string{
+	"56acf2ec5ae4906e8814369849f60d07ba339671e57f52200ab473e183463f2e",
+	"6b0fc7403ffa214357f0326224903e612acf5c3fc5b88dfc175a2be81e343609",
+	"6bed3bef2bb8b41289bca3e7f92fa5e7714decb404590ccbbc6ff7dcabf0c725",
+	"e4fb03a7eae2f3766f76d52719633a65c7882c7734ae7afe603e97d193f42c0e",
+	"53e784cb876751e114c9e4c1921240f184b6dff8d167715227e3708d0f7bb26d",
+	"27a53423aa3e5d5c46bf30be53a9998dd247daf758847f244f82d430be71de6e",
+}
+
+// GivenTXSpec is a builder for creating a MOCK! transaction
+type GivenTXSpec struct {
+	utxos   []*trx.UTXO
+	outputs []*trx.TransactionOutput
+	t       *testing.T
+	sign    bool
+
+	grandparentTXIndex int
+	sourceTransactions map[string]*trx.Transaction
+}
+
+// GivenTX creates a new GivenTXSpec for building a MOCK! transaction
+func GivenTX() *GivenTXSpec {
+	return &GivenTXSpec{
+		grandparentTXIndex: 0,
+		sourceTransactions: make(map[string]*trx.Transaction),
+	}
+}
+
+// WithT sets the testing.T instance to be used for error reporting
+func (spec *GivenTXSpec) WithT(t *testing.T) *GivenTXSpec {
+	spec.t = t
+	return spec
+}
+
+// WithSign enables signing of the transaction (default is false)
+func (spec *GivenTXSpec) WithSign() *GivenTXSpec {
+	spec.sign = true
+	return spec
+}
+
+// WithInput adds an input to the transaction with the specified satoshis
+// it automatically creates a parent tx (sourceTX) with P2PKH UTXO with provided satoshis
+func (spec *GivenTXSpec) WithInput(satoshis uint64) *GivenTXSpec {
+	sourceTX := spec.makeParentTX(satoshis)
+	utxo, err := trx.NewUTXO(sourceTX.TxID().String(), 0, P2PKHLockingScript.String(), satoshis)
+	if err != nil {
+		spec.error("creating utxo for input", err)
+	}
+	utxo.UnlockingScriptTemplate = P2PKHUnlockingScriptTemplate
+
+	spec.sourceTransactions[sourceTX.TxID().String()] = sourceTX
+	spec.utxos = append(spec.utxos, utxo)
+
+	return spec
+}
+
+// WithOPReturn adds an OP_RETURN output to the transaction with the specified data
+func (spec *GivenTXSpec) WithOPReturn(dataStr string) *GivenTXSpec {
+	data := []byte(dataStr)
+	o, err := trx.CreateOpReturnOutput([][]byte{data})
+	if err != nil {
+		spec.error("creating op return output", err)
+	}
+
+	spec.outputs = append(spec.outputs, o)
+
+	return spec
+}
+
+// WithOutputFunc can be used for adding custom outputs to the transaction
+func (spec *GivenTXSpec) WithOutputFunc(maker func() *trx.TransactionOutput) *GivenTXSpec {
+	spec.outputs = append(spec.outputs, maker())
+	return spec
+}
+
+// TX creates a go-sdk transaction from the given spec
+func (spec *GivenTXSpec) TX() *trx.Transaction {
+	tx := trx.NewTransaction()
+	if err := tx.AddInputsFromUTXOs(spec.utxos...); err != nil {
+		spec.error("adding inputs to tx", err)
+	}
+
+	for _, output := range spec.outputs {
+		tx.AddOutput(output)
+	}
+
+	//TODO consider to source inputs under some condition (spec param)
+	for _, input := range tx.Inputs {
+		if sourceTX := spec.sourceTransactions[input.SourceTXID.String()]; sourceTX != nil {
+			input.SourceTransaction = sourceTX
+		}
+	}
+
+	if spec.sign {
+		if err := tx.Sign(); err != nil {
+			spec.error("signing tx", tx, err)
+		}
+	}
+	return tx
+}
+
+// InputUTXO returns UTXO outpoint for the input with the specified index
+func (spec *GivenTXSpec) InputUTXO(inputID int) bsv.Outpoint {
+	return bsv.Outpoint{
+		TxID: spec.utxos[inputID].TxID.String(),
+		Vout: spec.utxos[inputID].Vout,
+	}
+}
+
+// ID returns the transaction ID
+func (spec *GivenTXSpec) ID() string {
+	return spec.TX().TxID().String()
+}
+
+// BeefHex returns the BEEF hex of the transaction
+func (spec *GivenTXSpec) BeefHex() string {
+	tx := spec.TX()
+	beef, err := tx.BEEFHex()
+	if err != nil {
+		spec.error("getting beef hex", err)
+	}
+	return beef
+}
+
+// RawTXHex returns the raw hex of the transaction
+func (spec *GivenTXSpec) RawTXHex() string {
+	tx := spec.TX()
+	return tx.Hex()
+}
+
+// EFHex returns the EF hex of the transaction
+func (spec *GivenTXSpec) EFHex() string {
+	tx := spec.TX()
+	ef, err := tx.EFHex()
+	if err != nil {
+		spec.error("getting ef hex", err)
+	}
+	return ef
+}
+
+func (spec *GivenTXSpec) error(args ...any) {
+	if spec.t != nil {
+		spec.t.Error(args...)
+	} else {
+		panic(args)
+	}
+}
+
+func (spec *GivenTXSpec) makeParentTX(satoshis uint64) *trx.Transaction {
+	tx := trx.NewTransaction()
+
+	withFee := satoshis + 1
+	utxo, err := trx.NewUTXO(spec.getNextGrandparentTXID(), 0, P2PKHLockingScript.String(), withFee)
+	if err != nil {
+		spec.error("creating utxo for source tx", err)
+	}
+	utxo.UnlockingScriptTemplate = P2PKHUnlockingScriptTemplate
+
+	err = tx.AddInputsFromUTXOs(utxo)
+	if err != nil {
+		spec.error("adding input to source tx", err)
+	}
+
+	tx.AddOutput(&trx.TransactionOutput{
+		Satoshis:      satoshis,
+		LockingScript: P2PKHLockingScript,
+	})
+	err = tx.Sign()
+	if err != nil {
+		spec.error("signing source tx", err)
+	}
+
+	err = tx.AddMerkleProof(trx.NewMerklePath(1000, [][]*trx.PathElement{{
+		&trx.PathElement{
+			Hash:   tx.TxID(),
+			Offset: 0,
+		},
+	}}))
+	if err != nil {
+		spec.error("adding merkle proof to source tx", err)
+	}
+
+	return tx
+}
+
+func (spec *GivenTXSpec) getNextGrandparentTXID() string {
+	id := grandparentTXIDs[spec.grandparentTXIndex]
+	spec.grandparentTXIndex = (spec.grandparentTXIndex + 1) % len(grandparentTXIDs)
+	return id
+}
+
+func ptr[T any](value T) *T {
+	return &value
+}
--- engine/transaction/record/record_outline_test.go
+++ engine/transaction/record/record_outline_test.go
@@ -4,8 +4,11 @@ import (
 	"context"
 	"testing"
 
+	"github.com/bitcoin-sv/go-sdk/script"
+	trx "github.com/bitcoin-sv/go-sdk/transaction"
 	"github.com/bitcoin-sv/spv-wallet/engine/database"
 	"github.com/bitcoin-sv/spv-wallet/engine/tester"
+	"github.com/bitcoin-sv/spv-wallet/engine/tester/fixtures"
 	"github.com/bitcoin-sv/spv-wallet/engine/transaction"
 	txerrors "github.com/bitcoin-sv/spv-wallet/engine/transaction/errors"
 	"github.com/bitcoin-sv/spv-wallet/engine/transaction/outlines"
@@ -16,26 +19,28 @@ import (
 )
 
 const (
-	beefWithOpReturn = "0100beef01fe47390d000802b402b23c7c47320b3818c665bf28a46c290f3fb379ea8d357625bbff3117ae14b09bb5003af2d2162c10310bee0e861e8a8dc94bd896d416e784aa22f667501ac270bcc5015b003640de1da50be7bcc0a7a6fd56f55cf70c9818394c2f99f71a43a777607b3d38012c0059c7771f2e6b337f2bd826b4f35b3159b801d6a8fe8c485c4eff9e902f341da8011700e24840f82e0d91356217fe8854eb28d70b347d231389a6c978d4c89eb3d034ac010a0064841631ac16075ff068de9bd4f4a4a1c74394284e584e7d3060b594be31d39b0104001097bf4eda72ca3d8092b7c7eb1e17251e377dff79a9104d24ee0a111f7119800103009ebf3de54b3d0380536be7938bb0d9ee9145d4a4a2ad96d65f3011b58424b844010000a09fdac73af0f273b576ac48d330362b7552a0508c987ecd26d4a455bf0a24ab02010000000238163d2fd3cb01c87476437f37428fd0680ec3fe89a7057678b00b0435fdddde010000006b483045022100c141a1b551dc3f9a2d30a4234933d6d6aa86a889a143a1e6d613713dc3f2ed68022038b631deef1b229f2da84536286cf5d5377c148849f54ccb07d7957ce22e732c412102ab62d27fc4692c260b30d856ab68d8ca82d4f1fe3aec805250d8f850faa71827ffffffff06656a05bcd886e933f99088ab038e416a0f1e28fd7d856dfdb4aa31d7335ed7010000006a473044022065025d2c32ae5fb2e53732b1c48d4e12b3cb14bb6e66ae535e779f94932d303e02201906963e4c37d6e73890e65f7944d53a233255f8ad95b8bd1ee420ff3cea87084121027249db5e1c879cdc4ed98dc24d3a94d64f364dbb9f84b039cd44e415ba282305ffffffff0202000000000000001976a914298b970788e84da5f82276638ad6c0204d7622a988ac12000000000000001976a914b69e83df1129f7f442c7e0d54c31681f64ae9d7f88ac0000000001000100000001b23c7c47320b3818c665bf28a46c290f3fb379ea8d357625bbff3117ae14b09b000000006a47304402205febda774d651f1b15dcb50b450fb10356ae25ab887e9023f35e440138b3f19b022027af299175e8a42649b03d110c1e031f6c20a1d9fb1fa72af813185fec4ae0be412102aaec858e0431eaf8a056d37a55639a18aa189e45eeb8bb94fe33880aa3ddd65effffffff0100000000000000000e006a0b68656c6c6f20776f726c640000000000"
-	txIDWithOpReturn = "af5d7b59b2973355a043c70e1fa0738bd0ceecd90af87fd52d4cdb88ac3eb10b"
 	dataOfOpReturnTx = "hello world"
-
-	prevTxID        = "9bb014ae1731ffbb2576358dea79b33f0f296ca428bf65c618380b32477c3cb2"
-	prevOutputIndex = 0
-
-	notSignedBeef = "0100beef01fe47390d000802b5003af2d2162c10310bee0e861e8a8dc94bd896d416e784aa22f667501ac270bcc5b402b23c7c47320b3818c665bf28a46c290f3fb379ea8d357625bbff3117ae14b09b015b003640de1da50be7bcc0a7a6fd56f55cf70c9818394c2f99f71a43a777607b3d38012c0059c7771f2e6b337f2bd826b4f35b3159b801d6a8fe8c485c4eff9e902f341da8011700e24840f82e0d91356217fe8854eb28d70b347d231389a6c978d4c89eb3d034ac010a0064841631ac16075ff068de9bd4f4a4a1c74394284e584e7d3060b594be31d39b0104001097bf4eda72ca3d8092b7c7eb1e17251e377dff79a9104d24ee0a111f7119800103009ebf3de54b3d0380536be7938bb0d9ee9145d4a4a2ad96d65f3011b58424b844010000a09fdac73af0f273b576ac48d330362b7552a0508c987ecd26d4a455bf0a24ab02010000000238163d2fd3cb01c87476437f37428fd0680ec3fe89a7057678b00b0435fdddde010000006b483045022100c141a1b551dc3f9a2d30a4234933d6d6aa86a889a143a1e6d613713dc3f2ed68022038b631deef1b229f2da84536286cf5d5377c148849f54ccb07d7957ce22e732c412102ab62d27fc4692c260b30d856ab68d8ca82d4f1fe3aec805250d8f850faa71827ffffffff06656a05bcd886e933f99088ab038e416a0f1e28fd7d856dfdb4aa31d7335ed7010000006a473044022065025d2c32ae5fb2e53732b1c48d4e12b3cb14bb6e66ae535e779f94932d303e02201906963e4c37d6e73890e65f7944d53a233255f8ad95b8bd1ee420ff3cea87084121027249db5e1c879cdc4ed98dc24d3a94d64f364dbb9f84b039cd44e415ba282305ffffffff0202000000000000001976a914298b970788e84da5f82276638ad6c0204d7622a988ac12000000000000001976a914b69e83df1129f7f442c7e0d54c31681f64ae9d7f88ac0000000001000100000001b23c7c47320b3818c665bf28a46c290f3fb379ea8d357625bbff3117ae14b09b0000000000ffffffff0100000000000000000e006a0b68656c6c6f20776f726c640000000000"
-	notABeefHex   = "0100000001b23c7c47320b3818c665bf28a46c290f3fb379ea8d357625bbff3117ae14b09b0000000000ffffffff0100000000000000000e006a0b68656c6c6f20776f726c6400000000"
+	notABeefHex      = "0100000001b23c7c47320b3818c665bf28a46c290f3fb379ea8d357625bbff3117ae14b09b0000000000ffffffff0100000000000000000e006a0b68656c6c6f20776f726c6400000000"
 
 	beefWithOnePTPKH = "0100beef01fe47390d000802b5003af2d2162c10310bee0e861e8a8dc94bd896d416e784aa22f667501ac270bcc5b402b23c7c47320b3818c665bf28a46c290f3fb379ea8d357625bbff3117ae14b09b015b003640de1da50be7bcc0a7a6fd56f55cf70c9818394c2f99f71a43a777607b3d38012c0059c7771f2e6b337f2bd826b4f35b3159b801d6a8fe8c485c4eff9e902f341da8011700e24840f82e0d91356217fe8854eb28d70b347d231389a6c978d4c89eb3d034ac010a0064841631ac16075ff068de9bd4f4a4a1c74394284e584e7d3060b594be31d39b0104001097bf4eda72ca3d8092b7c7eb1e17251e377dff79a9104d24ee0a111f7119800103009ebf3de54b3d0380536be7938bb0d9ee9145d4a4a2ad96d65f3011b58424b844010000a09fdac73af0f273b576ac48d330362b7552a0508c987ecd26d4a455bf0a24ab02010000000238163d2fd3cb01c87476437f37428fd0680ec3fe89a7057678b00b0435fdddde010000006b483045022100c141a1b551dc3f9a2d30a4234933d6d6aa86a889a143a1e6d613713dc3f2ed68022038b631deef1b229f2da84536286cf5d5377c148849f54ccb07d7957ce22e732c412102ab62d27fc4692c260b30d856ab68d8ca82d4f1fe3aec805250d8f850faa71827ffffffff06656a05bcd886e933f99088ab038e416a0f1e28fd7d856dfdb4aa31d7335ed7010000006a473044022065025d2c32ae5fb2e53732b1c48d4e12b3cb14bb6e66ae535e779f94932d303e02201906963e4c37d6e73890e65f7944d53a233255f8ad95b8bd1ee420ff3cea87084121027249db5e1c879cdc4ed98dc24d3a94d64f364dbb9f84b039cd44e415ba282305ffffffff0202000000000000001976a914298b970788e84da5f82276638ad6c0204d7622a988ac12000000000000001976a914b69e83df1129f7f442c7e0d54c31681f64ae9d7f88ac0000000001000100000001b23c7c47320b3818c665bf28a46c290f3fb379ea8d357625bbff3117ae14b09b000000006a47304402202a28b2fda3d37411189fb0d539f054e14b805a8a181d4bf7656fd8f8933fba3702207598eab7b1470ee98ae178a4d5a7bacde74e13e3d694295406051e620a409adc412102aaec858e0431eaf8a056d37a55639a18aa189e45eeb8bb94fe33880aa3ddd65effffffff0101000000000000001976a914f4838029c3838a94b5b7736f2fe82d860e0239df88ac0000000000"
-
-	// beefWithOpReturnWithoutOPFalse contains only OP_RETURN instead of standard OP_FALSE OP_RETURN
-	beefWithOpReturnWithoutOPFalse = "0100beef01fe47390d000802b5003af2d2162c10310bee0e861e8a8dc94bd896d416e784aa22f667501ac270bcc5b402b23c7c47320b3818c665bf28a46c290f3fb379ea8d357625bbff3117ae14b09b015b003640de1da50be7bcc0a7a6fd56f55cf70c9818394c2f99f71a43a777607b3d38012c0059c7771f2e6b337f2bd826b4f35b3159b801d6a8fe8c485c4eff9e902f341da8011700e24840f82e0d91356217fe8854eb28d70b347d231389a6c978d4c89eb3d034ac010a0064841631ac16075ff068de9bd4f4a4a1c74394284e584e7d3060b594be31d39b0104001097bf4eda72ca3d8092b7c7eb1e17251e377dff79a9104d24ee0a111f7119800103009ebf3de54b3d0380536be7938bb0d9ee9145d4a4a2ad96d65f3011b58424b844010000a09fdac73af0f273b576ac48d330362b7552a0508c987ecd26d4a455bf0a24ab02010000000238163d2fd3cb01c87476437f37428fd0680ec3fe89a7057678b00b0435fdddde010000006b483045022100c141a1b551dc3f9a2d30a4234933d6d6aa86a889a143a1e6d613713dc3f2ed68022038b631deef1b229f2da84536286cf5d5377c148849f54ccb07d7957ce22e732c412102ab62d27fc4692c260b30d856ab68d8ca82d4f1fe3aec805250d8f850faa71827ffffffff06656a05bcd886e933f99088ab038e416a0f1e28fd7d856dfdb4aa31d7335ed7010000006a473044022065025d2c32ae5fb2e53732b1c48d4e12b3cb14bb6e66ae535e779f94932d303e02201906963e4c37d6e73890e65f7944d53a233255f8ad95b8bd1ee420ff3cea87084121027249db5e1c879cdc4ed98dc24d3a94d64f364dbb9f84b039cd44e415ba282305ffffffff0202000000000000001976a914298b970788e84da5f82276638ad6c0204d7622a988ac12000000000000001976a914b69e83df1129f7f442c7e0d54c31681f64ae9d7f88ac0000000001000100000001b23c7c47320b3818c665bf28a46c290f3fb379ea8d357625bbff3117ae14b09b000000006a47304402203f6163dd452a7857d283f67acdeedd2626294f93d804c283d1538b0b494da888022034e4c6bb9cc5439e1b6e59726099f525aec3a64502d2a1922a22981bec6ffa0d412102aaec858e0431eaf8a056d37a55639a18aa189e45eeb8bb94fe33880aa3ddd65effffffff0100000000000000000d6a0b68656c6c6f20776f726c640000000000"
-	txIDWithOpReturnWithoutOPFalse = "61afaeadbe53fcde58179c7650c7fa4a004f677f481b8083a2bc38c1ebd694f5"
-
-	beefWithOpZeroAfterOpReturn = "0100beef01fe47390d000802b402b23c7c47320b3818c665bf28a46c290f3fb379ea8d357625bbff3117ae14b09bb5003af2d2162c10310bee0e861e8a8dc94bd896d416e784aa22f667501ac270bcc5015b003640de1da50be7bcc0a7a6fd56f55cf70c9818394c2f99f71a43a777607b3d38012c0059c7771f2e6b337f2bd826b4f35b3159b801d6a8fe8c485c4eff9e902f341da8011700e24840f82e0d91356217fe8854eb28d70b347d231389a6c978d4c89eb3d034ac010a0064841631ac16075ff068de9bd4f4a4a1c74394284e584e7d3060b594be31d39b0104001097bf4eda72ca3d8092b7c7eb1e17251e377dff79a9104d24ee0a111f7119800103009ebf3de54b3d0380536be7938bb0d9ee9145d4a4a2ad96d65f3011b58424b844010000a09fdac73af0f273b576ac48d330362b7552a0508c987ecd26d4a455bf0a24ab02010000000238163d2fd3cb01c87476437f37428fd0680ec3fe89a7057678b00b0435fdddde010000006b483045022100c141a1b551dc3f9a2d30a4234933d6d6aa86a889a143a1e6d613713dc3f2ed68022038b631deef1b229f2da84536286cf5d5377c148849f54ccb07d7957ce22e732c412102ab62d27fc4692c260b30d856ab68d8ca82d4f1fe3aec805250d8f850faa71827ffffffff06656a05bcd886e933f99088ab038e416a0f1e28fd7d856dfdb4aa31d7335ed7010000006a473044022065025d2c32ae5fb2e53732b1c48d4e12b3cb14bb6e66ae535e779f94932d303e02201906963e4c37d6e73890e65f7944d53a233255f8ad95b8bd1ee420ff3cea87084121027249db5e1c879cdc4ed98dc24d3a94d64f364dbb9f84b039cd44e415ba282305ffffffff0202000000000000001976a914298b970788e84da5f82276638ad6c0204d7622a988ac12000000000000001976a914b69e83df1129f7f442c7e0d54c31681f64ae9d7f88ac0000000001000100000001b23c7c47320b3818c665bf28a46c290f3fb379ea8d357625bbff3117ae14b09b000000006a47304402205febda774d651f1b15dcb50b450fb10356ae25ab887e9023f35e440138b3f19b022027af299175e8a42649b03d110c1e031f6c20a1d9fb1fa72af813185fec4ae0be412102aaec858e0431eaf8a056d37a55639a18aa189e45eeb8bb94fe33880aa3ddd65effffffff0100000000000000001c006a0b68656c6c6f20776f726c6400000b68656c6c6f20776f726c640000000000"
 )
 
 func TestRecordOutlineOpReturn(t *testing.T) {
+	givenTxWithOpReturn := fixtures.GivenTX().WithT(t).
+		WithSign().
+		WithInput(1).
+		WithOPReturn(dataOfOpReturnTx)
+
+	givenTxWithOpReturnWithoutOPFalse := fixtures.GivenTX().WithT(t).
+		WithSign().
+		WithInput(1).
+		WithOutputFunc(func() *trx.TransactionOutput {
+			s := &script.Script{}
+			_ = s.AppendOpcodes(script.OpRETURN)
+			_ = s.AppendPushDataArray([][]byte{[]byte(dataOfOpReturnTx)})
+			return &trx.TransactionOutput{LockingScript: s}
+		})
+
 	tests := map[string]struct {
 		repo          *mockRepository
 		outline       *outlines.Transaction
@@ -44,12 +49,9 @@ func TestRecordOutlineOpReturn(t *testing.T) {
 		expectData    []database.Data
 	}{
 		"RecordTransactionOutline for op_return": {
-			repo: newMockRepository().withOutput(database.Output{
-				TxID: prevTxID,
-				Vout: prevOutputIndex,
-			}),
+			repo: newMockRepository().withUTXO(givenTxWithOpReturn.InputUTXO(0)),
 			outline: &outlines.Transaction{
-				BEEF: beefWithOpReturn,
+				BEEF: givenTxWithOpReturn.BeefHex(),
 				Annotations: transaction.Annotations{
 					Outputs: transaction.OutputsAnnotations{
 						0: &transaction.OutputAnnotation{
@@ -58,34 +60,31 @@ func TestRecordOutlineOpReturn(t *testing.T) {
 					},
 				},
 			},
-			expectTxID: txIDWithOpReturn,
+			expectTxID: givenTxWithOpReturn.ID(),
 			expectOutputs: []database.Output{
 				{
-					TxID:       prevTxID,
-					Vout:       prevOutputIndex,
-					SpendingTX: ptr(txIDWithOpReturn),
+					TxID:       givenTxWithOpReturn.InputUTXO(0).TxID,
+					Vout:       givenTxWithOpReturn.InputUTXO(0).Vout,
+					SpendingTX: ptr(givenTxWithOpReturn.ID()),
 				},
 				{
-					TxID:       txIDWithOpReturn,
+					TxID:       givenTxWithOpReturn.ID(),
 					Vout:       0,
 					SpendingTX: nil,
 				},
 			},
 			expectData: []database.Data{
 				{
-					TxID: txIDWithOpReturn,
+					TxID: givenTxWithOpReturn.ID(),
 					Vout: 0,
 					Blob: []byte(dataOfOpReturnTx),
 				},
 			},
 		},
 		"RecordTransactionOutline for op_return without leading OP_FALSE": {
-			repo: newMockRepository().withOutput(database.Output{
-				TxID: prevTxID,
-				Vout: prevOutputIndex,
-			}),
+			repo: newMockRepository().withUTXO(givenTxWithOpReturnWithoutOPFalse.InputUTXO(0)),
 			outline: &outlines.Transaction{
-				BEEF: beefWithOpReturnWithoutOPFalse,
+				BEEF: givenTxWithOpReturnWithoutOPFalse.BeefHex(),
 				Annotations: transaction.Annotations{
 					Outputs: transaction.OutputsAnnotations{
 						0: &transaction.OutputAnnotation{
@@ -94,22 +93,22 @@ func TestRecordOutlineOpReturn(t *testing.T) {
 					},
 				},
 			},
-			expectTxID: txIDWithOpReturnWithoutOPFalse,
+			expectTxID: givenTxWithOpReturnWithoutOPFalse.ID(),
 			expectOutputs: []database.Output{
 				{
-					TxID:       prevTxID,
-					Vout:       prevOutputIndex,
-					SpendingTX: ptr(txIDWithOpReturnWithoutOPFalse),
+					TxID:       givenTxWithOpReturnWithoutOPFalse.InputUTXO(0).TxID,
+					Vout:       givenTxWithOpReturnWithoutOPFalse.InputUTXO(0).Vout,
+					SpendingTX: ptr(givenTxWithOpReturnWithoutOPFalse.ID()),
 				},
 				{
-					TxID:       txIDWithOpReturnWithoutOPFalse,
+					TxID:       givenTxWithOpReturnWithoutOPFalse.ID(),
 					Vout:       0,
 					SpendingTX: nil,
 				},
 			},
 			expectData: []database.Data{
 				{
-					TxID: txIDWithOpReturnWithoutOPFalse,
+					TxID: givenTxWithOpReturnWithoutOPFalse.ID(),
 					Vout: 0,
 					Blob: []byte(dataOfOpReturnTx),
 				},
@@ -118,7 +117,7 @@ func TestRecordOutlineOpReturn(t *testing.T) {
 		"RecordTransactionOutline for op_return with untracked utxo as inputs": {
 			repo: newMockRepository(),
 			outline: &outlines.Transaction{
-				BEEF: beefWithOpReturn,
+				BEEF: givenTxWithOpReturn.BeefHex(),
 				Annotations: transaction.Annotations{
 					Outputs: transaction.OutputsAnnotations{
 						0: &transaction.OutputAnnotation{
@@ -127,14 +126,14 @@ func TestRecordOutlineOpReturn(t *testing.T) {
 					},
 				},
 			},
-			expectTxID: txIDWithOpReturn,
+			expectTxID: givenTxWithOpReturn.ID(),
 			expectOutputs: []database.Output{{
-				TxID: txIDWithOpReturn,
+				TxID: givenTxWithOpReturn.ID(),
 				Vout: 0,
 			}},
 			expectData: []database.Data{
 				{
-					TxID: txIDWithOpReturn,
+					TxID: givenTxWithOpReturn.ID(),
 					Vout: 0,
 					Blob: []byte(dataOfOpReturnTx),
 				},
@@ -168,6 +167,29 @@ func TestRecordOutlineOpReturn(t *testing.T) {
 }
 
 func TestRecordOutlineOpReturnErrorCases(t *testing.T) {
+	givenUnsignedTX := fixtures.GivenTX().WithT(t).
+		WithInput(1).
+		WithOPReturn(dataOfOpReturnTx)
+
+	//Fixme: Apparently go-sdk doesn't check output scripts as we thought
+	//givenTxWithOpZeroAfterOpReturn := fixtures.GivenTX().WithT(t).
+	//	WithSign().
+	//	WithInput(1).
+	//	WithOutputFunc(func() *trx.TransactionOutput {
+	//		s := &script.Script{}
+	//		_ = s.AppendOpcodes(script.OpFALSE, script.OpRETURN)
+	//		_ = s.AppendPushDataArray([][]byte{[]byte(dataOfOpReturnTx)})
+	//		_ = s.AppendOpcodes(script.OpZERO)
+	//		_ = s.AppendOpcodes(script.OpZERO)
+	//		_ = s.AppendPushDataArray([][]byte{[]byte(dataOfOpReturnTx)})
+	//		return &trx.TransactionOutput{LockingScript: s}
+	//	})
+
+	givenTxWithOpReturn := fixtures.GivenTX().WithT(t).
+		WithSign().
+		WithInput(1).
+		WithOPReturn(dataOfOpReturnTx)
+
 	tests := map[string]struct {
 		repo        *mockRepository
 		outline     *outlines.Transaction
@@ -178,7 +200,7 @@ func TestRecordOutlineOpReturnErrorCases(t *testing.T) {
 			broadcaster: newMockBroadcaster(),
 			repo:        newMockRepository(),
 			outline: &outlines.Transaction{
-				BEEF: notSignedBeef,
+				BEEF: givenUnsignedTX.BeefHex(),
 			},
 			expectErr: txerrors.ErrTxValidation,
 		},
@@ -190,23 +212,24 @@ func TestRecordOutlineOpReturnErrorCases(t *testing.T) {
 			},
 			expectErr: txerrors.ErrTxValidation,
 		},
-		"RecordTransactionOutline for invalid OP_ZERO after OP_RETURN": {
-			broadcaster: newMockBroadcaster(),
-			repo:        newMockRepository(),
-			outline: &outlines.Transaction{
-				BEEF: beefWithOpZeroAfterOpReturn,
-			},
-			expectErr: txerrors.ErrTxValidation,
-		},
+		//Fixme: Apparently go-sdk doesn't check output scripts as we thought
+		//"RecordTransactionOutline for invalid OP_ZERO after OP_RETURN": {
+		//	broadcaster: newMockBroadcaster(),
+		//	repo:        newMockRepository(),
+		//	outline: &outlines.Transaction{
+		//		BEEF: givenTxWithOpZeroAfterOpReturn.BeefHex(),
+		//	},
+		//	expectErr: txerrors.ErrTxValidation,
+		//},
 		"Tx with already spent utxo": {
 			broadcaster: newMockBroadcaster(),
 			repo: newMockRepository().withOutput(database.Output{
-				TxID:       prevTxID,
-				Vout:       prevOutputIndex,
+				TxID:       givenTxWithOpReturn.InputUTXO(0).TxID,
+				Vout:       givenTxWithOpReturn.InputUTXO(0).Vout,
 				SpendingTX: ptr("05aa91319c773db18071310ecd5ddc15d3aa4242b55705a13a66f7fefe2b80a1"),
 			}),
 			outline: &outlines.Transaction{
-				BEEF: beefWithOpReturn,
+				BEEF: givenTxWithOpReturn.BeefHex(),
 			},
 			expectErr: txerrors.ErrUTXOSpent,
 		},
@@ -214,7 +237,7 @@ func TestRecordOutlineOpReturnErrorCases(t *testing.T) {
 			broadcaster: newMockBroadcaster(),
 			repo:        newMockRepository(),
 			outline: &outlines.Transaction{
-				BEEF: beefWithOpReturn,
+				BEEF: givenTxWithOpReturn.BeefHex(),
 				Annotations: transaction.Annotations{
 					Outputs: transaction.OutputsAnnotations{
 						1: &transaction.OutputAnnotation{
@@ -229,7 +252,7 @@ func TestRecordOutlineOpReturnErrorCases(t *testing.T) {
 			broadcaster: newMockBroadcaster(),
 			repo:        newMockRepository(),
 			outline: &outlines.Transaction{
-				BEEF: beefWithOpReturn,
+				BEEF: givenTxWithOpReturn.BeefHex(),
 				Annotations: transaction.Annotations{
 					Outputs: transaction.OutputsAnnotations{
 						-1: &transaction.OutputAnnotation{
@@ -258,11 +281,11 @@ func TestRecordOutlineOpReturnErrorCases(t *testing.T) {
 		"error during broadcasting": {
 			broadcaster: newMockBroadcaster().withError(errors.New("broadcast error")),
 			repo: newMockRepository().withOutput(database.Output{
-				TxID: prevTxID,
-				Vout: prevOutputIndex,
+				TxID: givenTxWithOpReturn.InputUTXO(0).TxID,
+				Vout: givenTxWithOpReturn.InputUTXO(0).Vout,
 			}),
 			outline: &outlines.Transaction{
-				BEEF: beefWithOpReturn,
+				BEEF: givenTxWithOpReturn.BeefHex(),
 				Annotations: transaction.Annotations{
 					Outputs: transaction.OutputsAnnotations{
 						0: &transaction.OutputAnnotation{
