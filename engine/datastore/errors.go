20a98f367c2dd305ceb727eedd36de3161f60a83
Damian-4chain <damian.zoltowski@4chain.studio>
2024-11-19 12:27:05 +0100

    feat(SPV-1008): remove datastore ErrNoResults

--- engine/datastore/errors.go
+++ engine/datastore/errors.go
@@ -7,9 +7,6 @@ import (
 // ErrUnsupportedEngine is used when the engine given is not a known datastore engine
 var ErrUnsupportedEngine = spverrors.Newf("unsupported datastore engine")
 
-// ErrNoResults error when no results are found
-var ErrNoResults = spverrors.Newf("no results found")
-
 // ErrUnsupportedDriver is when the given SQL driver is not determined to be known or supported
 var ErrUnsupportedDriver = spverrors.Newf("sql driver unsupported")
 
