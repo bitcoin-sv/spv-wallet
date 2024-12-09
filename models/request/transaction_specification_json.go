88feba23c6f26ddb3028297ecfda79857e33363d
Krzysztof Tomecki <152964795+chris-4chain@users.noreply.github.com>
2024-12-09 11:27:22 +0100

    fix(SPV-1069): fix styles

--- models/request/transaction_specification_json.go
+++ models/request/transaction_specification_json.go
@@ -29,7 +29,7 @@ func unmarshalOutput(rawOutput json.RawMessage, outputType string) (Output, erro
 	}
 }
 
-// expandOutputForMarshaling used by TransactionSpecification marshalling to expand Output object before marshalling.
+// expandOutputForMarshaling used by TransactionSpecification marshaling to expand Output object before marshaling.
 // IMPORTANT: Every time a new output type is added, it must be handled here also.
 func expandOutputForMarshaling(output Output) (any, error) {
 	switch o := output.(type) {
