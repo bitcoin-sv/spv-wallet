f9f967c7e1b4a60d2f0857a7e84956c324ada28b
jakubmkowalski <jakub.kowalski@4chain.studio>
2024-12-13 23:32:15 +0100

    fix(SPV-1214): fix linters errors

--- models/filter/contact_admin_filter.go
+++ models/filter/contact_admin_filter.go
@@ -2,7 +2,8 @@ package filter
 
 // AdminContactFilter extends ContactFilter for admin-specific use, including xpubid filtering
 type AdminContactFilter struct {
-	ContactFilter `mapstructure:",squash" json:"-"`
+	//nolint:staticcheck // SA5008 We want to reuse json tags also to mapstructure.
+	ContactFilter `json:",inline,squash"`
 	XPubID        *string `json:"xpubid,omitempty" example:"623bc25ce1c0fc510dea72b5ee27b2e70384c099f1f3dce9e73dd987198c3486"`
 }
 
