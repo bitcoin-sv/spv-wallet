178988e1cd6e6560ea5cf07dda19e92763767859
Krzysztof Tomecki <152964795+chris-4chain@users.noreply.github.com>
2025-01-14 07:11:00 +0100

    feat(SPV-1337): missing description comment for model UserInfo

--- models/response/user_info.go
+++ models/response/user_info.go
@@ -2,6 +2,7 @@ package response
 
 import "github.com/bitcoin-sv/spv-wallet/models/bsv"
 
+// UserInfo represents the response model for current user information
 type UserInfo struct {
 	CurrentBalance bsv.Satoshis `json:"currentBalance"`
 }
