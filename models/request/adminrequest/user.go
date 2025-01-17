8e87fb8b3dc8e7fc551ab34d601be0dcab5b822f
Krzysztof Tomecki <152964795+chris-4chain@users.noreply.github.com>
2025-01-17 10:40:08 +0100

    feat(SPV-1392): lint

--- models/request/adminrequest/user.go
+++ models/request/adminrequest/user.go
@@ -7,6 +7,7 @@ type CreateUser struct {
 	Paymail *AddPaymail `json:"paymail"` // creating paymail during user creation is optional
 }
 
+// PaymailDefined returns true if the optional paymail to add is defined in the request.
 func (r *CreateUser) PaymailDefined() bool {
 	return r.Paymail != nil
 }
--- models/response/adminresponse/paymail.go
+++ models/response/adminresponse/paymail.go
@@ -1,5 +1,6 @@
 package adminresponse
 
+// Paymail represents response data for a paymail.
 type Paymail struct {
 	ID uint `json:"id"`
 
