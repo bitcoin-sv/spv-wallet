d4c97023a95c64a5fb001d7a957b300a320037b3
Damian Orzepowski <damian.orzepowski@4chain.studio>
2025-01-20 15:09:32 +0100

    feat(SPV-1387, SPV-1396): replace estimated unlocking script size with estimated input size; rename UsersUTXO (#857)

--- engine/database/user_utxos.go
+++ engine/database/user_utxos.go
@@ -6,29 +6,39 @@ import (
 	"gorm.io/datatypes"
 )
 
-// UserUtxos is a table holding user's Unspent Transaction Outputs (UTXOs).
-// TODO: It should be renamed to UserUTXO.
-type UserUtxos struct {
-	UserID                       string `gorm:"primaryKey;uniqueIndex:idx_window,sort:asc,priority:1"`
-	TxID                         string `gorm:"primaryKey;uniqueIndex:idx_window,sort:asc,priority:4"`
-	Vout                         uint32 `gorm:"primaryKey;uniqueIndex:idx_window,sort:asc,priority:5"`
-	Satoshis                     uint64
-	UnlockingScriptEstimatedSize uint64
-	Bucket                       string    `gorm:"check:chk_not_data_bucket,bucket <> 'data'"`
-	CreatedAt                    time.Time `gorm:"uniqueIndex:idx_window,sort:asc,priority:3"`
-	TouchedAt                    time.Time `gorm:"uniqueIndex:idx_window,sort:asc,priority:2"`
-	CustomInstructions           datatypes.JSONSlice[CustomInstruction]
+// EstimatedInputSizeForP2PKH is the estimated size increase when adding and unlocking P2PKH input to transaction.
+// 32 bytes txID
+// + 4 bytes vout index
+// + 1 byte script length
+// + 107 bytes script pub key
+// + 4 bytes nSequence
+const EstimatedInputSizeForP2PKH = 148
+
+// UserUTXO is a table holding user's Unspent Transaction Outputs (UTXOs).
+type UserUTXO struct {
+	UserID   string `gorm:"primaryKey;uniqueIndex:idx_window,sort:asc,priority:1"`
+	TxID     string `gorm:"primaryKey;uniqueIndex:idx_window,sort:asc,priority:4"`
+	Vout     uint32 `gorm:"primaryKey;uniqueIndex:idx_window,sort:asc,priority:5"`
+	Satoshis uint64
+	// EstimatedInputSize is the estimated size increase when adding and unlocking this UTXO to a transaction.
+	EstimatedInputSize uint64
+	Bucket             string    `gorm:"check:chk_not_data_bucket,bucket <> 'data'"`
+	CreatedAt          time.Time `gorm:"uniqueIndex:idx_window,sort:asc,priority:3"`
+	// TouchedAt is the time when the UTXO was last touched (selected for preparing transaction outline) - used for prioritizing UTXO selection.
+	TouchedAt time.Time `gorm:"uniqueIndex:idx_window,sort:asc,priority:2"`
+	// CustomInstructions is the list of instructions for unlocking given UTXO (it should be understood by client).
+	CustomInstructions datatypes.JSONSlice[CustomInstruction]
 }
 
-// NewP2PKHUserUTXO creates a new UserUtxos instance for a P2PKH output based on the given output and custom instructions.
-func NewP2PKHUserUTXO(output *TrackedOutput, customInstructions datatypes.JSONSlice[CustomInstruction]) *UserUtxos {
-	return &UserUtxos{
-		UserID:                       output.UserID,
-		TxID:                         output.TxID,
-		Vout:                         output.Vout,
-		Satoshis:                     uint64(output.Satoshis),
-		UnlockingScriptEstimatedSize: 106,
-		Bucket:                       "bsv",
-		CustomInstructions:           customInstructions,
+// NewP2PKHUserUTXO creates a new UserUTXO instance for a P2PKH output based on the given output and custom instructions.
+func NewP2PKHUserUTXO(output *TrackedOutput, customInstructions datatypes.JSONSlice[CustomInstruction]) *UserUTXO {
+	return &UserUTXO{
+		UserID:             output.UserID,
+		TxID:               output.TxID,
+		Vout:               output.Vout,
+		Satoshis:           uint64(output.Satoshis),
+		EstimatedInputSize: EstimatedInputSizeForP2PKH,
+		Bucket:             "bsv",
+		CustomInstructions: customInstructions,
 	}
 }
--- engine/tester/fixtures/tx_const_fixtures.go
+++ engine/tester/fixtures/tx_const_fixtures.go
@@ -7,6 +7,3 @@ var DefaultFeeUnit = bsv.FeeUnit{
 	Satoshis: 1,
 	Bytes:    1000,
 }
-
-// EstimatedUnlockingScriptSizeForP2PKH is the estimated unlocking script size for a P2PKH transaction.
-const EstimatedUnlockingScriptSizeForP2PKH = 106
