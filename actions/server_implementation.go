706f3099edb421b3ca23f84ef51fcc76c6a8eea1
Krzysztof Tomecki <152964795+chris-4chain@users.noreply.github.com>
2025-01-20 14:36:01 +0100

    feat(spike): self review

--- actions/server_implementation.go
+++ actions/server_implementation.go
@@ -4,14 +4,13 @@ import (
 	"github.com/bitcoin-sv/spv-wallet/api"
 )
 
-// optional code omitted
-
 // Server is the implementation of the server oapi-codegen's interface
 type Server struct {
 	AdminServer
 	UserServer
 }
 
+// check if the Server implements the interface api.ServerInterface
 var _ api.ServerInterface = &Server{}
 
 // NewServer creates a new server
