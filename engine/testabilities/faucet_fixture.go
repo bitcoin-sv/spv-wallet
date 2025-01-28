0f81522fb05f44568dfd19b48566b4a5efe8ddea
Krzysztof Tomecki <152964795+chris-4chain@users.noreply.github.com>
2025-01-28 08:25:12 +0100

    feat(SPV-1121): remove "2" suffix

--- engine/testabilities/faucet_fixture.go
+++ engine/testabilities/faucet_fixture.go
@@ -6,7 +6,7 @@ import (
 
 	"github.com/bitcoin-sv/spv-wallet/engine"
 	"github.com/bitcoin-sv/spv-wallet/engine/tester/fixtures"
-	txmodels2 "github.com/bitcoin-sv/spv-wallet/engine/v2/transaction/txmodels"
+	"github.com/bitcoin-sv/spv-wallet/engine/v2/transaction/txmodels"
 	"github.com/bitcoin-sv/spv-wallet/models/bsv"
 	"github.com/stretchr/testify/assert"
 )
@@ -29,17 +29,17 @@ func (f *faucetFixture) TopUp(satoshis bsv.Satoshis) fixtures.GivenTXSpec {
 		WithRecipient(f.user).
 		WithP2PKHOutput(uint64(satoshis))
 
-	operation := txmodels2.NewOperation{
+	operation := txmodels.NewOperation{
 		UserID: f.user.ID(),
 
 		Type:  "incoming",
 		Value: int64(satoshis), //nolint:gosec // This is a test fixture, values won't exceed int64
 
-		Transaction: &txmodels2.NewTransaction{
+		Transaction: &txmodels.NewTransaction{
 			ID:       txSpec.ID(),
-			TxStatus: txmodels2.TxStatusMined,
-			Outputs: []txmodels2.NewOutput{
-				txmodels2.NewOutputForP2PHK(
+			TxStatus: txmodels.TxStatusMined,
+			Outputs: []txmodels.NewOutput{
+				txmodels.NewOutputForP2PHK(
 					bsv.Outpoint{TxID: txSpec.ID(), Vout: 0},
 					f.user.ID(),
 					satoshis,
@@ -49,7 +49,7 @@ func (f *faucetFixture) TopUp(satoshis bsv.Satoshis) fixtures.GivenTXSpec {
 		},
 	}
 
-	err := f.engine.Repositories().Operations.SaveAll(context.Background(), func(yield func(*txmodels2.NewOperation) bool) {
+	err := f.engine.Repositories().Operations.SaveAll(context.Background(), func(yield func(*txmodels.NewOperation) bool) {
 		yield(&operation)
 	})
 	f.assert.NoError(err)
--- engine/v2/transaction/record/interface.go
+++ engine/v2/transaction/record/interface.go
@@ -7,7 +7,7 @@ import (
 	trx "github.com/bitcoin-sv/go-sdk/transaction"
 	"github.com/bitcoin-sv/spv-wallet/engine/chain/models"
 	"github.com/bitcoin-sv/spv-wallet/engine/v2/addresses/addressesmodels"
-	txmodels2 "github.com/bitcoin-sv/spv-wallet/engine/v2/transaction/txmodels"
+	"github.com/bitcoin-sv/spv-wallet/engine/v2/transaction/txmodels"
 	"github.com/bitcoin-sv/spv-wallet/models/bsv"
 )
 
@@ -18,12 +18,12 @@ type AddressesService interface {
 
 // OutputsRepo is an interface for outputs repository.
 type OutputsRepo interface {
-	FindByOutpoints(ctx context.Context, outpoints iter.Seq[bsv.Outpoint]) ([]txmodels2.TrackedOutput, error)
+	FindByOutpoints(ctx context.Context, outpoints iter.Seq[bsv.Outpoint]) ([]txmodels.TrackedOutput, error)
 }
 
 // OperationsRepo is an interface for operations repository.
 type OperationsRepo interface {
-	SaveAll(ctx context.Context, opRows iter.Seq[*txmodels2.NewOperation]) error
+	SaveAll(ctx context.Context, opRows iter.Seq[*txmodels.NewOperation]) error
 }
 
 // Broadcaster is an interface for broadcasting transactions.
