987a5fb25f3c754cb1f545d90f81b8b007f832a3
Damian-4chain <damian.zoltowski@4chain.studio>
2025-01-30 14:12:09 +0100

    feat(SPV-1433): fix lint

--- models/request/opreturn/output.go
+++ models/request/opreturn/output.go
@@ -10,14 +10,3 @@ type Output struct {
 func (o Output) GetType() string {
 	return "op_return"
 }
-
-// Output is a struct that represents the output containing OP_RETURN script
-type OutputT struct {
-	DataType DataType `json:"dataType,omitempty"`
-	Data     []string `json:"data"`
-}
-
-// GetType returns the string typename of the output
-func (o OutputT) GetType() string {
-	return "opT_return"
-}
