755e368fd77becfc4654d109dccc4a3c397df20f
Krzysztof Tomecki <152964795+chris-4chain@users.noreply.github.com>
2025-02-03 10:23:19 +0100

    feat(SPV-1168): double spending

---
+++ actions/v2/transactions/outlines_record_doublespanding_test.go
@@ -0,0 +1,106 @@
+package transactions_test
+
+import (
+	"github.com/bitcoin-sv/spv-wallet/actions/testabilities"
+	"github.com/bitcoin-sv/spv-wallet/actions/testabilities/apierror"
+	chainmodels "github.com/bitcoin-sv/spv-wallet/engine/chain/models"
+	testengine "github.com/bitcoin-sv/spv-wallet/engine/testabilities"
+	"github.com/bitcoin-sv/spv-wallet/engine/tester/fixtures"
+	"testing"
+)
+
+func TestDoubleSpending(t *testing.T) {
+	// given:
+	givenForAllTests := testabilities.Given(t)
+	cleanup := givenForAllTests.StartedSPVWalletWithConfiguration(
+		testengine.WithV2(),
+	)
+	defer cleanup()
+
+	// and:
+	sourceTxSpec := givenForAllTests.Faucet(fixtures.Sender).TopUp(1000)
+
+	t.Run("Spending the UTXO", func(t *testing.T) {
+		// given:
+		given, then := testabilities.NewOf(givenForAllTests, t)
+
+		// and:
+		txSpec := fixtures.GivenTX(t).
+			WithSender(fixtures.Sender).
+			WithInputFromUTXO(sourceTxSpec.TX(), 0).
+			WithOPReturn("hello world")
+
+		// and:
+		client := given.HttpClient().ForUser()
+
+		// and:
+		request := `{
+			"hex": "` + txSpec.BEEF() + `",
+			"annotations": {
+				"outputs": {
+					"0": {
+						"bucket": "data"
+					}
+				}
+			}
+		}`
+
+		// and:
+		given.ARC().WillRespondForBroadcast(200, &chainmodels.TXInfo{
+			TxID:     txSpec.ID(),
+			TXStatus: chainmodels.SeenOnNetwork,
+		})
+
+		// when:
+		res, _ := client.R().
+			SetHeader("Content-Type", "application/json").
+			SetBody(request).
+			Post(transactionsOutlinesRecordURL)
+
+		// then:
+		then.Response(res).
+			HasStatus(201).
+			WithJSONMatching(`{
+				"txID": "{{ .txID }}"
+			}`, map[string]any{
+				"txID": txSpec.ID(),
+			})
+	})
+
+	t.Run("Double spend attempt", func(t *testing.T) {
+		// given:
+		given, then := testabilities.NewOf(givenForAllTests, t)
+
+		// and:
+		txSpec := fixtures.GivenTX(t).
+			WithSender(fixtures.Sender).
+			WithInputFromUTXO(sourceTxSpec.TX(), 0).
+			WithOPReturn("other data")
+
+		// and:
+		client := given.HttpClient().ForUser()
+
+		// and:
+		request := `{
+			"hex": "` + txSpec.BEEF() + `",
+			"annotations": {
+				"outputs": {
+					"0": {
+						"bucket": "data"
+					}
+				}
+			}
+		}`
+
+		// when:
+		res, _ := client.R().
+			SetHeader("Content-Type", "application/json").
+			SetBody(request).
+			Post(transactionsOutlinesRecordURL)
+
+		// then:
+		then.Response(res).
+			HasStatus(400).
+			WithJSONf(apierror.ExpectedJSON("error-utxo-spent", "UTXO is already spent"))
+	})
+}
