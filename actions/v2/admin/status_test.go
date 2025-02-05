21372a719b2381b5baaefe24149ac42e51d1698e
Paweł Lewandowski <plewandowski555@gmail.com>
2025-02-05 14:48:17 +0100

    tests(SPV-1471): add test for new endpoint

---
+++ actions/v2/admin/status_test.go
@@ -0,0 +1,54 @@
+package admin_test
+
+import (
+	"github.com/bitcoin-sv/spv-wallet/actions/testabilities"
+	testengine "github.com/bitcoin-sv/spv-wallet/engine/testabilities"
+	"testing"
+)
+
+func TestStatus(t *testing.T) {
+	givenForAllTests := testabilities.Given(t)
+	cleanup := givenForAllTests.StartedSPVWalletWithConfiguration(
+		testengine.WithV2(),
+	)
+	defer cleanup()
+
+	t.Run("Get admin-status as admin", func(t *testing.T) {
+		// given:
+		given, then := testabilities.NewOf(givenForAllTests, t)
+		client := given.HttpClient().ForAdmin()
+
+		// when:
+		res, _ := client.R().Get("/api/v2/admin/status")
+
+		// then:
+		then.Response(res).
+			IsOK()
+	})
+
+	t.Run("Try to get admin-status as user", func(t *testing.T) {
+		// given:
+		given, then := testabilities.NewOf(givenForAllTests, t)
+		client := given.HttpClient().ForUser()
+
+		// when:
+		res, _ := client.R().Get("/api/v2/admin/status")
+
+		// then:
+		then.Response(res).
+			IsUnauthorizedForUser()
+	})
+
+	t.Run("Try to get admin-status as anonymous", func(t *testing.T) {
+		// given:
+		given, then := testabilities.NewOf(givenForAllTests, t)
+		client := given.HttpClient().ForAnonymous()
+
+		// when:
+		res, _ := client.R().Get("/api/v2/admin/status")
+
+		// then:
+		then.Response(res).
+			IsUnauthorized()
+	})
+}
