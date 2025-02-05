a14dbf1783722e73147e74d5931a1129a64cc372
Paweł Lewandowski <plewandowski555@gmail.com>
2025-02-05 10:01:51 +0100

    chore(SPV-1471): igonre generated yaml in linter

--- actions/v2/admin/status.go
+++ actions/v2/admin/status.go
@@ -6,8 +6,10 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+// AdminServer represents server with API endpoints
 type AdminServer struct{}
 
+// GetApiV2AdminStatus return the status of the server only after admin authentication
 func (s *AdminServer) GetApiV2AdminStatus(c *gin.Context) {
 	c.Status(http.StatusOK)
 }
