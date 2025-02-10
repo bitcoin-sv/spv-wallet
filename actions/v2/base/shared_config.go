9e0dc1ff73ea8e4337b0bfaa01bca680ccde2de7
Paweł Lewandowski <plewandowski555@gmail.com>
2025-02-10 15:38:06 +0100

    chore: use config from struct to return shared config

--- actions/v2/base/shared_config.go
+++ actions/v2/base/shared_config.go
@@ -1,26 +1,20 @@
 package base
 
 import (
-	"net/http"
-	"sync"
-
-	"github.com/bitcoin-sv/spv-wallet/api"
-	"github.com/bitcoin-sv/spv-wallet/server/reqctx"
+	"github.com/bitcoin-sv/spv-wallet/models/response"
 	"github.com/gin-gonic/gin"
+	"net/http"
 )
 
 // GetApiV2ConfigsShared is the handler for SharedConfig which can be obtained by both admin and user
 func (s *APIBase) GetApiV2ConfigsShared(c *gin.Context) {
-	appconfig := reqctx.AppConfig(c)
-	makeConfig := sync.OnceValue(func() api.ApiComponentsResponsesSharedConfig {
-		return api.ApiComponentsResponsesSharedConfig{
-			PaymailDomains: &appconfig.Paymail.Domains,
-			ExperimentalFeatures: &map[string]bool{
-				"pikeContactsEnabled": appconfig.ExperimentalFeatures.PikeContactsEnabled,
-				"pikePaymentEnabled":  appconfig.ExperimentalFeatures.PikePaymentEnabled,
-			},
-		}
-	})
+	sharedConfig := response.SharedConfig{
+		PaymailDomains: s.config.Paymail.Domains,
+		ExperimentalFeatures: map[string]bool{
+			"pikeContactsEnabled": s.config.ExperimentalFeatures.PikeContactsEnabled,
+			"pikePaymentEnabled":  s.config.ExperimentalFeatures.PikePaymentEnabled,
+		},
+	}
 
-	c.JSON(http.StatusOK, makeConfig())
+	c.JSON(http.StatusOK, sharedConfig)
 }
