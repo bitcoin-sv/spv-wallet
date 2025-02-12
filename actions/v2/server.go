94941832900ee2dd25a6938ae12a6f18df4c0b85
Paweł Lewandowski <35259896+pawellewandowski98@users.noreply.github.com>
2025-02-12 09:44:11 +0100

    feat(SPV-1471): add configs shared  v2 endpoint (#897)

--- actions/v2/server.go
+++ actions/v2/server.go
@@ -2,18 +2,24 @@ package v2
 
 import (
 	"github.com/bitcoin-sv/spv-wallet/actions/v2/admin"
+	"github.com/bitcoin-sv/spv-wallet/actions/v2/base"
 	"github.com/bitcoin-sv/spv-wallet/api"
+	"github.com/bitcoin-sv/spv-wallet/config"
 )
 
 // Server is the implementation of the server oapi-codegen's interface
 type Server struct {
-	admin.Server
+	admin.APIAdmin
+	base.APIBase
 }
 
 // check if the Server implements the interface api.ServerInterface
 var _ api.ServerInterface = &Server{}
 
 // NewServer creates a new server
-func NewServer() *Server {
-	return &Server{}
+func NewServer(config *config.AppConfig) *Server {
+	return &Server{
+		APIAdmin: *admin.NewAPIAdmin(),
+		APIBase:  *base.NewAPIBase(config),
+	}
 }
