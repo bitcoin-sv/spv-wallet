f6f532c122472cb667304d6e8327bd067ff72967
Krzysztof Tomecki <152964795+chris-4chain@users.noreply.github.com>
2025-02-24 12:16:28 +0100

    feat(SPV-1555): more cases for with_change

--- actions/v2/transactions/outlines_record_withchange_test.go
+++ actions/v2/transactions/outlines_record_withchange_test.go
@@ -1,87 +1,167 @@
 package transactions_test
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/bitcoin-sv/spv-wallet/actions/testabilities"
 	testengine "github.com/bitcoin-sv/spv-wallet/engine/testabilities"
 	"github.com/bitcoin-sv/spv-wallet/engine/tester/fixtures"
 	"github.com/bitcoin-sv/spv-wallet/models/bsv"
+	"github.com/samber/lo"
 )
 
 func TestTransactionWithChange(t *testing.T) {
-	// given:
-	given, then := testabilities.New(t)
-	cleanup := given.StartedSPVWalletWithConfiguration(
-		testengine.WithV2(),
-	)
-	defer cleanup()
-
-	// and:
-	sender := fixtures.Sender
-
-	// and:
-	sourceTxSpec := given.Faucet(sender).TopUp(1001)
-
-	// and:
-	changeCustomInstr := []bsv.CustomInstruction{
-		{
-			Type:        "type42",
-			Instruction: "1-destination-cb15dc54d06387d3be7bd26cfe2b5996",
-		},
-	}
-
-	// and:
-	txSpec := fixtures.GivenTX(t).
-		WithSender(sender).
-		WithInputFromUTXO(sourceTxSpec.TX(), 0).
-		WithOPReturn("hello, world").
-		WithOutputScript(1000, sender.P2PKHLockingScript(changeCustomInstr...))
-
-	// and:
-	client := given.HttpClient().ForGivenUser(sender)
-
-	// and:
-	given.ARC().WillRespondForBroadcastWithSeenOnNetwork(txSpec.ID())
-
-	// when:
-	res, _ := client.R().
-		SetHeader("Content-Type", "application/json").
-		SetBody(map[string]any{
-			"hex":    txSpec.BEEF(),
-			"format": "BEEF",
-			"annotations": map[string]any{
-				"outputs": map[string]any{
-					"0": map[string]any{
-						"bucket": "data",
+	tests := map[string]struct {
+		changeOutputs changeOutputSpecs
+	}{
+		"one change output": {
+			changeOutputs: changeOutputSpecs{
+				{
+					customInstructions: bsv.CustomInstructions{
+						{
+							Type:        "type42",
+							Instruction: "1-destination-1output4d06387d3be7bd26cfe2b5996",
+						},
 					},
-					"1": map[string]any{
-						"bucket":             "bsv",
-						"customInstructions": changeCustomInstr,
-					},
+					satoshis: 1000,
 				},
 			},
-		}).
-		Post(transactionsOutlinesRecordURL)
+		},
+		"two change outputs": {
+			changeOutputs: changeOutputSpecs{
+				{
+					customInstructions: bsv.CustomInstructions{
+						{
+							Type:        "type42",
+							Instruction: "1-destination-1output4d06387d3be7bd26cfe2b5996",
+						},
+					},
+					satoshis: 600,
+				},
+				{
+					customInstructions: bsv.CustomInstructions{
+						{
+							Type:        "type42",
+							Instruction: "1-destination-2output4d06387d3be7bd26cfe2b5996",
+						},
+					},
+					satoshis: 400,
+				},
+			},
+		},
+		"one change outputs with longer custom instructions": {
+			changeOutputs: changeOutputSpecs{
+				{
+					customInstructions: bsv.CustomInstructions{
+						{
+							Type:        "type42",
+							Instruction: "1-destination-1instruction87d3be7bd26cfe2b5996",
+						},
+						{
+							Type:        "type42",
+							Instruction: "1-destination-2instruction7d3be7bd26cfe2b5996",
+						},
+						{
+							Type:        "type42",
+							Instruction: "1-destination-3instruction87d3be7bd26cfe2b5996",
+						},
+					},
+					satoshis: 1000,
+				},
+			},
+		},
+	}
+	for name, test := range tests {
+		t.Run(name, func(t *testing.T) {
+			// given:
+			given, then := testabilities.New(t)
+			cleanup := given.StartedSPVWalletWithConfiguration(
+				testengine.WithV2(),
+			)
+			defer cleanup()
 
-	// then:
-	then.Response(res).
-		IsCreated().
-		WithJSONMatching(`{
+			// and:
+			sender := fixtures.Sender
+
+			// and:
+			sourceTxSpec := given.Faucet(sender).TopUp(1001)
+
+			// and:
+			txSpec := fixtures.GivenTX(t).
+				WithSender(sender).
+				WithInputFromUTXO(sourceTxSpec.TX(), 0).
+				WithOPReturn("hello, world")
+
+			for _, changeOutput := range test.changeOutputs {
+				txSpec.WithOutputScript(uint64(changeOutput.satoshis), sender.P2PKHLockingScript(changeOutput.customInstructions...))
+			}
+			// and:
+			client := given.HttpClient().ForGivenUser(sender)
+
+			// and:
+			given.ARC().WillRespondForBroadcastWithSeenOnNetwork(txSpec.ID())
+
+			// when:
+			res, _ := client.R().
+				SetHeader("Content-Type", "application/json").
+				SetBody(map[string]any{
+					"hex":    txSpec.BEEF(),
+					"format": "BEEF",
+					"annotations": map[string]any{
+						"outputs": lo.Assign(
+							map[string]any{
+								"0": map[string]any{
+									"bucket": "data",
+								},
+							},
+							test.changeOutputs.toAnnotations(1),
+						),
+					},
+				}).
+				Post(transactionsOutlinesRecordURL)
+
+			// then:
+			then.Response(res).
+				IsCreated().
+				WithJSONMatching(`{
 				"txID": "{{ .txID }}"
 			}`, map[string]any{
-			"txID": txSpec.ID(),
+					"txID": txSpec.ID(),
+				})
+
+			// and:
+			then.User(sender).Balance().IsEqualTo(1000)
+
+			// and:
+			then.User(sender).Operations().Last().
+				WithTxID(txSpec.ID()).
+				WithTxStatus("BROADCASTED").
+				WithValue(-1).
+				WithType("outgoing").
+				WithNoCounterparty()
 		})
+	}
+}
 
-	// and:
-	then.User(sender).Balance().IsEqualTo(1000)
+type changeOutputSpec struct {
+	customInstructions bsv.CustomInstructions
+	satoshis           bsv.Satoshis
+}
 
-	// and:
-	then.User(sender).Operations().Last().
-		WithTxID(txSpec.ID()).
-		WithTxStatus("BROADCASTED").
-		WithValue(-1).
-		WithType("outgoing").
-		WithNoCounterparty()
+func (ch changeOutputSpec) toAnnotation() map[string]any {
+	return map[string]any{
+		"bucket":             "bsv",
+		"customInstructions": ch.customInstructions,
+	}
+}
 
+type changeOutputSpecs []changeOutputSpec
+
+func (chs changeOutputSpecs) toAnnotations(startingIndex int) map[string]any {
+	annotations := make(map[string]any)
+	for i, ch := range chs {
+		annotations[fmt.Sprintf("%d", i+startingIndex)] = ch.toAnnotation()
+	}
+	return annotations
 }
