ccbc1b0098c224745be03a308b106b9bd707374d
Damian Orzepowski <damian.orzepowski@4chain.studio>
2025-02-25 08:54:01 +0100

    refactor: extract bad request user data to method

--- api/manualtests/adminapi/errorcases/admin_create_user_badreq_test.go
+++ api/manualtests/adminapi/errorcases/admin_create_user_badreq_test.go
@@ -64,14 +64,7 @@ func TestCreateUserBadRequest(t *testing.T) {
 			c, err := state.AdminClient()
 			require.NoError(t, err)
 
-			user := manualtests.User{
-				Alias:     "badrequest",
-				Domain:    state.Domain,
-				Xpriv:     "xprv9s21ZrQH143K3TzwskUB1NW5iHx4EH7cxquXfwpFR5HrWe6HQrYvECJsj3sg1DJhWhwjtw5WdXwje8pkyuuvzJUingwo4f5BkD5dNubfNUn",
-				Xpub:      "xpub661MyMwAqRbcFx5Qyn1BNWSpGKnYdjqUL4q8ULDryQpqPSRRxPsAmzdMaLnoyUzLAQ5ukXgMZjYs5LfNfsPFwBoSwxChePB1DxKvyFz6F67",
-				PublicKey: "035dcb59eb7b5c5982ba6fbbccffbb2460f8daa07c5f9a21f2c2cf0845dcc6dda9",
-				ID:        "1QCibNnc8CK7bzTdupM91f4PKixhMqqJQw",
-			}
+			user := UserDefinitionForMakingBadRequests(state.Domain)
 
 			req := test.makeRequest(user, state, t)
 			res, err := c.CreateUserWithResponse(context.Background(), req)
