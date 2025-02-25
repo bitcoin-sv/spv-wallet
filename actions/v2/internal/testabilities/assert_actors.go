bb6f81dc46f908a43f19efcecfe8c7a76a1de573
Damian Orzepowski <damian.orzepowski@4chain.studio>
2025-02-25 13:55:34 +0100

    test(SPV-1549): proposition to use interfaces (#929)

--- actions/v2/internal/testabilities/assert_actors.go
+++ actions/v2/internal/testabilities/assert_actors.go
@@ -1,20 +1,58 @@
 package testabilities
 
-import "github.com/bitcoin-sv/spv-wallet/actions/testabilities"
+import (
+	"testing"
 
-// Then starts an assertion chain for the specified actor
-func (tc *ActorTestContext) Then(actor *User) ActorAssertions {
-	return ActorAssertions{
-		userAssertions: tc.baseAssertions.User(actor.User),
+	"github.com/bitcoin-sv/spv-wallet/actions/testabilities"
+)
+
+type IntegrationTestAssertion interface {
+	Alice() SpvWalletActorsStateAssertions
+	Bob() SpvWalletActorsStateAssertions
+}
+
+// SpvWalletActorsStateAssertions about spv-wallet users
+type SpvWalletActorsStateAssertions interface {
+	Balance() testabilities.BalanceAssertions
+	Operations() testabilities.OperationsAssertions
+}
+
+type assertions struct {
+	t       testing.TB
+	fixture *fixture
+	testabilities.SPVWalletApplicationAssertions
+}
+
+func (a *assertions) Alice() SpvWalletActorsStateAssertions {
+	return &actorAssertions{
+		userAssertions: a.User(a.fixture.alice.User),
 	}
 }
 
+func (a *assertions) Bob() SpvWalletActorsStateAssertions {
+	return &actorAssertions{
+		userAssertions: a.User(a.fixture.bob.User),
+	}
+}
+
+func newAssertions(t testing.TB, fixture *fixture, appAssertions testabilities.SPVWalletApplicationAssertions) IntegrationTestAssertion {
+	return &assertions{
+		t:                              t,
+		fixture:                        fixture,
+		SPVWalletApplicationAssertions: appAssertions,
+	}
+}
+
+type actorAssertions struct {
+	userAssertions testabilities.SPVWalletAppUserAssertions
+}
+
 // Balance returns balance assertions for the actor
-func (a ActorAssertions) Balance() testabilities.BalanceAssertions {
+func (a actorAssertions) Balance() testabilities.BalanceAssertions {
 	return a.userAssertions.Balance()
 }
 
 // Operations returns operation assertions for the actor
-func (a ActorAssertions) Operations() testabilities.OperationsAssertions {
+func (a actorAssertions) Operations() testabilities.OperationsAssertions {
 	return a.userAssertions.Operations()
 }
