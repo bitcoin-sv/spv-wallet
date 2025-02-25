44f9d2f000cf5189b116c8dc215f3340751977ad
Krzysztof Tomecki <152964795+chris-4chain@users.noreply.github.com>
2025-02-25 14:16:10 +0100

    fix(SPV-1561): AfterFind to trim spaces for spendingTX

--- engine/v2/database/tracked_output.go
+++ engine/v2/database/tracked_output.go
@@ -1,6 +1,8 @@
 package database
 
 import (
+	"gorm.io/gorm"
+	"strings"
 	"time"
 
 	"github.com/bitcoin-sv/spv-wallet/models/bsv"
@@ -18,4 +20,14 @@ type TrackedOutput struct {
 
 	CreatedAt time.Time
 	UpdatedAt time.Time
+}
+
+// AfterFind is a GORM hook that is called after retrieving the record from the database.
+func (o *TrackedOutput) AfterFind(_ *gorm.DB) (err error) {
+	// Trim left spaces from SpendingTX
+	// Because the field is char(64) in the database, it will be padded with spaces.
+	// For some reason the value is padded only on postgres,
+	// so if changing, make sure to check it on all databases.
+	o.SpendingTX = strings.TrimLeft(o.SpendingTX, " ")
+	return nil
 }
