c986adf9b6629f2b99b237d1f2aad09065478efc
wregulski <wojciech.regulski@4chain.studio>
2025-02-27 15:44:15 +0100

    todolist

--- actions/v2/internal/integrationtests/spend_external_funds_op_return_test.go
+++ actions/v2/internal/integrationtests/spend_external_funds_op_return_test.go
@@ -2,3 +2,7 @@ package integrationtests
 
 // add testcase that we will receive external funds and send it via opreturn
 // separated method for opreturn
+
+// 1. Create SendsTo method (second one) with abstractions to allow create custom outputs etc
+// 2. Create separated method for SendsOpReturn
+// 3. And use it here
