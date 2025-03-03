1f3a690618018d144cd35e7c00a5f47ae8b494a5
Damian-4chain <damian.zoltowski@4chain.studio>
2025-03-03 12:14:59 +0100

    feat(SPV-1425): revert error return

--- engine/v2/paymails/paymails_service.go
+++ engine/v2/paymails/paymails_service.go
@@ -41,7 +41,7 @@ func (s *Service) Create(ctx context.Context, newPaymail *paymailsmodels.NewPaym
 	}
 
 	if err := newPaymail.ValidateAvatar(); err != nil {
-		return nil, err
+		return nil, spverrors.Wrapf(err, "invalid avatar url during paymail creation")
 	}
 	if newPaymail.PublicName == "" {
 		newPaymail.PublicName = newPaymail.Alias
