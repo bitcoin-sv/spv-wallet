ae4633dad6d60e996709ab2ee3714ee8d3091497
Damian Orzepowski <damian.orzepowski@4chain.studio>
2025-03-04 09:21:05 +0100

    feat(SPV-1570): add possibility to split paymail payment into several outputs. (#936)

--- engine/tester/jsonrequire/xpath.go
+++ engine/tester/jsonrequire/xpath.go
@@ -1,6 +1,7 @@
 package jsonrequire
 
 import (
+	"encoding/json"
 	"fmt"
 	"regexp"
 	"strconv"
@@ -26,26 +27,26 @@ func getByXPath(t testing.TB, data map[string]any, path string) any {
 			mapKey := matches[1]
 			index, err := strconv.Atoi(matches[2])
 			if err != nil {
-				require.Fail(t, fmt.Sprintf("invalid array index '%s'", matches[2]))
+				failOnGettingXpath(t, fmt.Sprintf("invalid array index '%s'", matches[2]), data, path)
 			}
 
 			m, ok := current.(map[string]any)
 			if !ok {
-				require.Fail(t, "path does not exist or is not a map")
+				failOnGettingXpath(t, "path does not exist or is not a map", data, path)
 			}
 
 			array, exists := m[mapKey]
 			if !exists {
-				require.Fail(t, fmt.Sprintf("key '%s' not found", mapKey))
+				failOnGettingXpath(t, fmt.Sprintf("key '%s' not found", mapKey), data, path)
 			}
 
 			slice, ok := array.([]any)
 			if !ok {
-				require.Fail(t, fmt.Sprintf("key '%s' is not an array", mapKey))
+				failOnGettingXpath(t, fmt.Sprintf("key '%s' is not an array", mapKey), data, path)
 			}
 
 			if index < 0 || index >= len(slice) {
-				require.Fail(t, fmt.Sprintf("index '%d' out of bounds for key '%s'", index, mapKey))
+				failOnGettingXpath(t, fmt.Sprintf("index '%d' out of bounds for key '%s'", index, mapKey), data, path)
 			}
 
 			// Move to the array element
@@ -54,16 +55,25 @@ func getByXPath(t testing.TB, data map[string]any, path string) any {
 			// Normal map traversal
 			m, ok := current.(map[string]any)
 			if !ok {
-				require.Fail(t, "path does not exist or is not a map")
+				failOnGettingXpath(t, "path does not exist or is not a map", data, path)
 			}
 
 			val, exists := m[key]
 			if !exists {
-				require.Fail(t, fmt.Sprintf("key '%s' not found", key))
+				failOnGettingXpath(t, fmt.Sprintf("key '%s' not found", key), data, path)
 			}
 			current = val
 		}
 	}
 
 	return current
+}
+
+func failOnGettingXpath(t testing.TB, failureMessage string, data map[string]any, path string) {
+	t.Helper()
+	jsonData, err := json.MarshalIndent(data, "", "  ")
+	if err != nil {
+		require.Failf(t, failureMessage, "Failed to get path %s from response %+v", path, data)
+	}
+	require.Fail(t, failureMessage, "Failed to get path %s from response %+v", path, string(jsonData))
 }
